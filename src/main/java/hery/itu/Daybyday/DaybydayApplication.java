676c11208221464c7c7c60c8dadc4d883646de49
Hery <153767421+Hery0019@users.noreply.github.com>
2025-03-23 14:47:07 +0300

    init

---
+++ src/main/java/hery/itu/Daybyday/DaybydayApplication.java
@@ -0,0 +1,13 @@
+package hery.itu.Daybyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaybydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaybydayApplication.class, args);
+	}
+
+}
